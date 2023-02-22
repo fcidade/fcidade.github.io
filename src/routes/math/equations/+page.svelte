53f38ed55fa5d47eb792a68d274377ef32a1f9fe
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2023-02-22 20:06:26 -0300

    simple equation

---
+++ src/routes/math/equations/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import SimpleBlogContent from '../../components/SimpleBlogContent.svelte';
+	import SimpleBlogHeader from '../../components/SimpleBlogHeader.svelte';
+
+	let valueA = 0;
+	let valueB = 0;
+	let x = 0;
+
+	let msg = '';
+
+	function generate() {
+		msg = '';
+		valueA = Math.floor(Math.random() * 10);
+		valueB = Math.floor(Math.random() * 10);
+	}
+
+	function confirm() {
+		const result = valueA === x + valueB;
+		msg = result ? 'Acertou!' : 'Errou!';
+	}
+
+	onMount(() => {
+		generate();
+	});
+</script>
+
+<SimpleBlogHeader title="Equações 🦾" description="Mais fácil do que parece" />
+
+<SimpleBlogContent>
+	<h2>{msg}</h2>
+
+	<p>
+		{valueA} = x + {valueB}
+	</p>
+
+	<p>Qual o valor de <b>x</b>?</p>
+
+	<form on:submit={confirm}>
+		<input type="number" bind:value={x} />
+		<button>Confirmar</button>
+		<button type="button" on:click={generate}>Novo</button>
+	</form>
+</SimpleBlogContent>
